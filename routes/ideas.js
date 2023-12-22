5548ed8bff8eb00ba15bccb7001ba1d9f723e3c3
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-12-22 15:26:03 -0600

    add update request

--- routes/ideas.js
+++ routes/ideas.js
@@ -5,21 +5,21 @@ const ideas = [
   {
     id: 1,
     text: "Positive NewsLetter, a newsletter that only shares positive, uplifting news",
-    tags: "Technology",
+    tag: "Technology",
     username: "TonyStark",
     date: "2023-01-02",
   },
   {
     id: 2,
     text: "Milk cartons that turn a different color the older that your milk is getting",
-    tags: "Inventions",
+    tag: "Inventions",
     username: "SteveRogers",
     date: "2023-01-02",
   },
   {
     id: 3,
     text: "ATM location app wich lets you know where the closest ATM is and if it is in service",
-    tags: "Software",
+    tag: "Software",
     username: "BruceBanner",
     date: "2023-01-02",
   },
@@ -52,6 +52,20 @@ router.post("/", (req, res) => {
   };
 
   ideas.push(idea);
+
+  res.json({ succes: true, data: idea });
+});
+
+// Update idea
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const idea = ideas.find((idea) => idea.id === +id);
+
+  if (!idea)
+    return res.status(404).json({ succes: false, error: "Resource not found" });
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
 
   res.json({ succes: true, data: idea });
 });
