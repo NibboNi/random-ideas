b4051c917cafb0740c3565a7ecc164595dd34404
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-12-29 21:24:27 -0600

    add modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,29 @@
+class Modal {
+  #modal;
+  #modalBtn;
+
+  constructor() {
+    this.#modal = document.querySelector("#modal");
+    this.#modalBtn = document.querySelector("#modal-btn");
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.#modalBtn.addEventListener("click", this.open.bind(this));
+    window.addEventListener("click", this.outsideClick.bind(this));
+  }
+
+  open() {
+    this.#modal.style.display = "block";
+  }
+
+  close() {
+    this.#modal.style.display = "none";
+  }
+
+  outsideClick(e) {
+    if (e.target === this.#modal) this.close();
+  }
+}
+
+export default Modal;
